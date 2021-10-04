e3e619086b55b4926d86713dcf0971abddb7ea1f
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-10-04 01:09:48 -0700

    box_filter: 3x3 works

--- P3/box_filter.cpp
+++ P3/box_filter.cpp
@@ -25,25 +25,24 @@ void applyKernel(cv::Mat image, cv::Mat kernel, cv::Mat &result)
 {
     result.create(image.size(),image.type());
     float sum = 0.0;
-    std::cout << image.rows << std::endl;
-        std::cout << image.cols << std::endl;
+    
+    //std::cout << image.rows << std::endl;
+    //std::cout << image.cols << std::endl;
 
     int r = kernel.rows/2;
 
-    for(int y = 0; y<result.rows; y++)
+    for(int y = 1; y<result.rows-1; y++)
     {
-        for(int x = 0; x<result.cols; x++)
+        for(int x = 1; x<result.cols-1; x++)
         {
-                //std::cout << x << std::endl;
-
-            for(int j = -1; j< kernel.rows-1; j++)
+            for(int j = -r; j< kernel.rows-r; j++)
             {
-                for(int i = -1; i<kernel.cols-1; i++)
+                for(int i = -r; i<kernel.cols-r; i++)
                 {
-                    if( ( (x-i)>0 ) && ( (y-j)>0 ) /*&&  ( (x-1+i)<result.cols ) && ( (y-1+j)>result.rows ) */)
+                    if( ( (x-i)>0 ) && ( (y-j)>0 ) && ( (x-i)<result.cols ) && ( (y-j)<result.rows ) )
                     {
-                        sum += kernel.at<float>( (j+1), (i+1) ) * image.at<uchar>( (y-j), (x-i) );
-                        //sum += kernel.at<float>(j, i) * image.at<uchar>(y-j+2*r, x-i+2*r);
+                        sum += kernel.at<float>( (j+r), (i+r) ) * image.at<uchar>( (y-j-1), (x-i-1) );
+                        //std::cout <<"SUM: " << sum << std::endl;
                     }
                 }
             }
@@ -52,8 +51,9 @@ void applyKernel(cv::Mat image, cv::Mat kernel, cv::Mat &result)
             sum = 0.0;
         }
     }
-     cv::namedWindow("Result");
-        cv::imshow("Result", result);
+    
+    cv::namedWindow("Result");
+    cv::imshow("Result", result);
 }
 
 
@@ -67,9 +67,15 @@ int main(int argc, char **argv)
             return 0;
         }
 
-        cv::Mat image = cv::imread(argv[1]/*, cv::IMREAD_GRAYSCALE*/);
+        int size = 0;
 
-        cv::Mat kernel = cv::Mat::zeros(3, 3, CV_32F);
+        std::cout << "Introduce the filter size: ";
+        std::cin >> size;
+
+
+        cv::Mat image = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+
+        cv::Mat kernel = cv::Mat::zeros(size, size, CV_32FC1);
         kernel = 1./9.;
 
         cv::Mat result;
