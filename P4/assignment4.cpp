6e442b400ff04e4e20e0ec2dce195dbec2ae1cf7
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-10-04 03:04:05 -0700

    assignment4: not completed at all

--- P4/assignment4.cpp
+++ P4/assignment4.cpp
@@ -25,19 +25,20 @@ void applyKernel(cv::Mat image, cv::Mat kernel, cv::Mat &result)
     result.create(image.size(),image.type());
     float sum = 0.0;
 
-    int r = kernel.rows/2;
+    //int r = kernel.rows/2;
 
     for(int y = 1; y<result.rows-1; y++)
     {
         for(int x = 1; x<result.cols-1; x++)
         {
-            for(int j = -r; j< kernel.rows-r; j++)
+            for(int j = -1; j< kernel.rows-1; j++)
             {
-                for(int i = -r; i<kernel.cols-r; i++)
+                for(int i = -1; i<kernel.cols-1; i++)
                 {
                     if( ( (x-i)>0 ) && ( (y-j)>0 ) && ( (x-i)<result.cols ) && ( (y-j)<result.rows ) )
                     {
-                        sum += kernel.at<float>( (j+r), (i+r) ) * image.at<uchar>( (y-j-1), (x-i-1) );
+                        sum += sqrt( pow( (kernel.at<float>( (j+1), (i+1) ) * image.at<uchar>( (y-j-1), (x-i-1) )), 2 ) +
+                                pow( (kernel.at<float>( (i+1), (j+1) ) * image.at<uchar>( (y-j-1), (x-i-1) )), 2 ) );
                     }
                 }
             }
@@ -61,14 +62,29 @@ int main(int argc, char **argv)
 
         int size = 0;
 
-        std::cout << "Introduce the filter size: ";
-        std::cin >> size;
+        /*std::cout << "Introduce the filter size: ";
+        std::cin >> size;*/
 
 
         cv::Mat image = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+        /*cv::Mat transformed = image.clone();
 
-        cv::Mat kernel = cv::Mat::zeros(size, size, CV_32FC1);
+        image.convertTo(transformed, CV_32FC1, 255, 0);*/
 
+        cv::Mat kernelX = cv::Mat::zeros(3, 3, CV_32FC1);
+        //cv::Mat kernelY = cv::Mat::zeros(3, 3, CV_32FC1);
+
+        float v1[] = {1, 2 , 1};
+        float v2[] = {-1, 0, 1};
+
+        for(int i = 0; i<=2; i++)
+        {
+            for(int j = 0; j<=2; j++)
+            {
+                kernelX.at<float>(i, j) = v1[i] * v2[j] ;
+            }
+        }
+        
         cv::Mat result;
 
         if(image.rows == 0)
@@ -77,7 +93,7 @@ int main(int argc, char **argv)
             return 0;
         }
 
-        applyKernel(image, kernel, result);
+        applyKernel(image, kernelX, result);
 
         cv::namedWindow("Original Image");
         cv::imshow("Original Image", image);
