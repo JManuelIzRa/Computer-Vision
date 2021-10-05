0ae4e61b64abbc44c3040caa8c156abf8ab4ee02
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-10-05 03:03:48 -0700

    assignment 4: loop

--- P4/assignment4.cpp
+++ P4/assignment4.cpp
@@ -59,11 +59,14 @@ void applyKernel(cv::Mat img, cv::Mat kernel, cv::Mat &result)
     sobel_y[1][0] = 0; sobel_y[1][1] = 0; sobel_y[1][2] = 0;
     sobel_y[2][0] = 1; sobel_y[2][1] = 2; sobel_y[2][2] = 1;
 
+    int value_x;
+    int value_y;
 
     for (int j = 0; j<img.rows-2; j++)
     {
         for (int i = 0; i<img.cols-2; i++)
         {
+
             int pixval_x =
             (sobel_x[0][0] * (int)img.at<uchar>(j,i)) + (sobel_x[0][1] * (int)img.at<uchar>(j+1,i)) + (sobel_x[0][2] * (int)img.at<uchar>(j+2,i)) +
             (sobel_x[1][0] * (int)img.at<uchar>(j,i+1)) + (sobel_x[1][1] * (int)img.at<uchar>(j+1,i+1)) + (sobel_x[1][2] * (int)img.at<uchar>(j+2,i+1)) +
@@ -85,7 +88,7 @@ void applyKernel(cv::Mat img, cv::Mat kernel, cv::Mat &result)
             }
 
             result.at<uchar>(j,i) = sum;
-     }
+        }
     }
 }
 
