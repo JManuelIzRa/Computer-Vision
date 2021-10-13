568b01de24019e56f4b6ec99191333a01b97fc6e
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-10-13 01:52:55 -0700

    assignment6: finished

--- P6/assignment6_Color.cpp
+++ P6/assignment6_Color.cpp
@@ -8,6 +8,35 @@ using namespace std;
 
 void medianFilter(const cv::Mat &in,cv::Mat &out, int size);
 bool sortFunction (int i,int j) { return (i<j); }
+void applyKernel(cv::Mat image, cv::Mat kernel, cv::Mat &result);
+
+void applyKernel(cv::Mat image, cv::Mat kernel, cv::Mat &result)
+{
+    result.create(image.size(),image.type());
+    float sum = 0.0;
+
+    int r = kernel.rows/2;
+
+    for(int y = 1; y<result.rows-1; y++)
+    {
+        for(int x = 1; x<result.cols-1; x++)
+        {
+            for(int j = -r; j< kernel.rows-r; j++)
+            {
+                for(int i = -r; i<kernel.cols-r; i++)
+                {
+                    if( ( (x-i)>0 ) && ( (y-j)>0 ) && ( (x-i)<result.cols ) && ( (y-j)<result.rows ) )
+                    {
+                        sum += kernel.at<float>( (j+r), (i+r) ) * image.at<uchar>( (y-j-1), (x-i-1) );
+                    }
+                }
+            }
+
+            result.at<uchar>(y, x) = sum;
+            sum = 0.0;
+        }
+    }
+}
 
 //Using split function and merge one
 void medianFilter(const cv::Mat &in,cv::Mat &out, int size)
@@ -68,31 +97,50 @@ int main (int argc, char** argv)
             return 0;
         }
 
-        cv::Mat result;
-        
         int size;
 
         std::cout << "Introduce the filter size: ";
         std::cin >> size;
 
+        //CREATION OF BOX FILTER KERNEL
+        cv::Mat kernel = cv::Mat::zeros(size, size, CV_32FC1);
+        
+        float k_size = size * size;
+        
+        kernel = 1./k_size;
+        //END OF
+
+        cv::Mat result_boxfilter;
+        cv::Mat result_medianblur;
+        
+        applyKernel( image, kernel, result_boxfilter );
         
         cv::Mat bgrchannel[3];
         
-        cv::Mat result_filter[3];
+        cv::Mat result_channel_mFilter[3];
+        cv::Mat result_channel_bFilter[3];
 
         cv::split(image, bgrchannel);//We split the channels and store them on different Mats
 
-        medianFilter( bgrchannel[0], result_filter[0], size );
-        medianFilter( bgrchannel[1], result_filter[1], size );
-        medianFilter( bgrchannel[2], result_filter[2], size );
+        medianFilter( bgrchannel[0], result_channel_mFilter[0], size );
+        medianFilter( bgrchannel[1], result_channel_mFilter[1], size );
+        medianFilter( bgrchannel[2], result_channel_mFilter[2], size );
 
-        cv::merge(result_filter, 3, result);
+        applyKernel( bgrchannel[0] , kernel, result_channel_bFilter[0] );
+        applyKernel( bgrchannel[1] , kernel, result_channel_bFilter[1] );
+        applyKernel( bgrchannel[2] , kernel, result_channel_bFilter[2] );
+
+        cv::merge(result_channel_mFilter, 3, result_medianblur);
+        cv::merge(result_channel_bFilter, 3, result_boxfilter);
 
         cv::namedWindow("Original Image");
         cv::imshow("Original Image", image);
 
-        cv::namedWindow("Result");
-        cv::imshow("Result", result);
+        cv::namedWindow("Result Box Filter");
+        cv::imshow("Result Box Filter", result_boxfilter);
+
+        cv::namedWindow("Result Median Blur");
+        cv::imshow("Result Median Blur", result_medianblur);
 
         cv::waitKey(0);
 
