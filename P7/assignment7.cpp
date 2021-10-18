4e0873dd40af81c71a197b89d7a62b55aa4f488b
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-10-18 01:16:57 -0700

    assignment7: completed

--- P7/assignment7.cpp
+++ P7/assignment7.cpp
@@ -11,7 +11,11 @@ using namespace std;
 
 void threshold(cv::Mat in, int threshold_value, cv::Mat &out);
 void erode(cv::Mat in, int k, cv::Mat &out);
+void dilate(cv::Mat in, int k, cv::Mat &out);
+void open(cv::Mat erode, cv::Mat dilate, cv::Mat &morph_ops);
+void close(cv::Mat dilate, cv::Mat erode,cv::Mat &morph_ops);
 bool sortFunction (int i,int j) { return (i<j); }
+bool sortFunctionMaxMin (int i, int j) { return (i>j); }
 
 
 void threshold(cv::Mat in, int threshold_value, cv::Mat &out)
@@ -42,11 +46,11 @@ void erode(cv::Mat in, int k, cv::Mat &out)
     {
         for(int x = 1; x<in.cols-1; x++)
         {
-            for(int j = -r; j< out.rows-r; j++)
+            for(int j = -r; j< k-r; j++)
             {
-                for(int i = -r; i<out.cols-r; i++)
+                for(int i = -r; i< k-r; i++)
                 {
-                    if( ( (x-i)>0 ) && ( (y-j)>0 ) && ( (x-i)<result.cols ) && ( (y-j)<result.rows ) )
+                    if( ( (x-i)>0 ) && ( (y-j)>0 ) && ( (x-i)<out.cols ) && ( (y-j)<out.rows ) )
                     {
                         v.push_back( in.at<uchar>( (y-j-1), (x-i-1) ) );
                     }
@@ -61,6 +65,66 @@ void erode(cv::Mat in, int k, cv::Mat &out)
     }
 }
 
+void dilate(cv::Mat in, int k, cv::Mat &out)
+{
+    out.create(in.size(), in.type());
+
+    int r = k/2;
+
+    int neighborhood = k*k;
+
+    std::vector<uchar> v(0, k*k);
+
+    for(int y = 1; y<in.rows-1; y++)
+    {
+        for(int x = 1; x<in.cols-1; x++)
+        {
+            for(int j = -r; j< k-r; j++)
+            {
+                for(int i = -r; i< k-r; i++)
+                {
+                    if( ( (x-i)>0 ) && ( (y-j)>0 ) && ( (x-i)<out.cols ) && ( (y-j)<out.rows ) )
+                    {
+                        v.push_back( in.at<uchar>( (y-j-1), (x-i-1) ) );
+                    }
+                }
+            }
+
+            std::sort (v.begin(), v.end(), sortFunctionMaxMin);
+            out.at<uchar>(y,x) = v[0];
+
+            v.clear();
+        }
+    }
+}
+
+void open(cv::Mat erode, cv::Mat dilate, cv::Mat &morph_ops)
+{
+    morph_ops.create(erode.size(), erode.type());
+
+    for(int y = 1; y<morph_ops.rows-1; y++)
+    {
+        for(int x = 1; x<morph_ops.cols-1; x++)
+        {
+            morph_ops.at<uchar>(y,x) = erode.at<uchar>(y,x)+dilate.at<uchar>(y,x);
+        }
+    }
+}
+
+void close(cv::Mat dilate, cv::Mat erode, cv::Mat &morph_ops)
+{
+    morph_ops.create(dilate.size(), dilate.type());
+
+    for(int y = 1; y<morph_ops.rows-1; y++)
+    {
+        for(int x = 1; x<morph_ops.cols-1; x++)
+        {
+            morph_ops.at<uchar>(y,x) = dilate.at<uchar>(y,x)+erode.at<uchar>(y,x);
+        }
+    }
+}
+
+
 int main ( int argc, char** argv)
 {
     
@@ -74,6 +138,8 @@ int main ( int argc, char** argv)
     
         cv::Mat image = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
         cv::Mat threshold_image;
+        cv::Mat erode_image;
+        cv::Mat dilate_image;
         cv::Mat morph_ops;
 
         if ( image.rows == 0 )
@@ -99,22 +165,32 @@ int main ( int argc, char** argv)
             string option = cml("-op");
             cout<<"-op option is in the command line\n";
 
+            int size;
+
+            std::cout << "Introduce the filter size: ";
+            std::cin >> size;
+
             if ( option == "erode" )
             {
                 cout<<"erode option is in the command line\n";
-                erode(threshold_image, 4, morph_ops);
+                erode(threshold_image, size, morph_ops);
             }
             else if ( option == "dilate" )
             {
                 cout<<"dilate option is in the command line\n";
+                dilate(threshold_image, size, morph_ops);
             }
             else if ( option == "open" )
             {
                 cout<<"open option is in the command line\n";
+                erode(threshold_image, size, erode_image);
+                dilate(erode_image, size, morph_ops);
             }
             else if ( option == "close" )
             {
                 cout<<"close option is in the command line\n";
+                dilate(threshold_image, size, dilate_image);
+                erode(dilate_image, size, morph_ops);
             }
             else
             {
@@ -129,6 +205,8 @@ int main ( int argc, char** argv)
 
         cv::namedWindow("Morph");
         cv::imshow("Morph", morph_ops);
+
+        cv::imwrite(argv[2], morph_ops);
         
         cv::waitKey(0);
     }
